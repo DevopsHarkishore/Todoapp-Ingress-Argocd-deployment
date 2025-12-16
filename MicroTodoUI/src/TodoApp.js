22c7594dc4008bd55e0107303de7582512f3703a
Harkishore Singh <harkishoresingh@gmail.com>
2025-12-16 23:18:07 +0530

    Update TodoApp.js

--- MicroTodoUI/src/TodoApp.js
+++ MicroTodoUI/src/TodoApp.js
@@ -21,7 +21,7 @@ function TodoApp() {
 
     const fetchTasks = async () => {
         try {
-            const response = await axios.get(`${GET_TASKS_API_BASE_URL}/tasks`);
+            const response = await axios.get(${GET_TASKS_API_BASE_URL}/tasks);
             setTasks(response.data);
         } catch (error) {
             console.error('Error fetching tasks', error);
